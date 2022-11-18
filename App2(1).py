99d7ce48061839c4601d6d3539b4892193031f1e
FENDOSIMO <118464464+FENDOSIMO@users.noreply.github.com>
2022-11-18 13:47:56 +0300

    Update App2(1).py

--- App2(1).py
+++ App2(1).py
@@ -12,30 +12,15 @@ from keras.models import load_model
 savedModel=load_model('gru_modelll.h5')
 savedModel.summary()
 
-
-
 def main():
-    st.title("Safaricom shares Price Prediction")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Price of shares Prediction ML App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    Day= st.number_input('Day:', min_value=1, max_value=2000, value=1)
-    result=""
-    if st.button("Predict"):
-        result=savedModel.predict(Day)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
-
-if __name__=='__main__':
-    main()
-
-
-# In[ ]:
+    st.title("Safaricom Shares Price Prediction Application")
+    st.header('Enter the Day to Predict Price:')
+    Day = st.number_input('Day Number:', min_value=0, max_value=1000, value=1)
+    if st.button('Predict'):
+        st.code(savedModel.predict(Day))
+        st.success(f'The predicted Price of Shares is {prediction[0]:.2f}')
+if __name__=='__main__': 
+        main()
 
 
 
